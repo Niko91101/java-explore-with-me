0515cf931acb9abbd5d641a42e145af334c126db
Stanislav Muzulev <muz-stanislav@yandex.ru>
2025-09-07 21:58:36 +0300

    fix: last 2 tests

--- ewm-stats-service/stats-server/src/main/java/ru/practicum/stats/server/exception/GlobalExceptionHandler.java
+++ ewm-stats-service/stats-server/src/main/java/ru/practicum/stats/server/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package ru.practicum.stats.server.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
@@ -13,7 +14,6 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
 
     private ResponseEntity<Map<String, Object>> body(HttpStatus status, String message) {
         return ResponseEntity.status(status).body(Map.of(
@@ -29,9 +29,14 @@ public class GlobalExceptionHandler {
         return body(HttpStatus.BAD_REQUEST, ex.getMessage());
     }
 
-    @ExceptionHandler({ MethodArgumentNotValidException.class, HttpMessageNotReadableException.class,
-            MethodArgumentTypeMismatchException.class })
+    @ExceptionHandler({MethodArgumentNotValidException.class, HttpMessageNotReadableException.class,
+            MethodArgumentTypeMismatchException.class})
     public ResponseEntity<Map<String, Object>> handleValidation(Exception ex) {
+        return body(HttpStatus.BAD_REQUEST, ex.getMessage());
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<Map<String, Object>> handleMissingParam(MissingServletRequestParameterException ex) {
         return body(HttpStatus.BAD_REQUEST, ex.getMessage());
     }
 
